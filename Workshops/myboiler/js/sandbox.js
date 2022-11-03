a35915a9c911b88ae99e86e0d020cf450447b04f
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-11-03 15:49:02 +0100

    Done with the netninja challenge

--- Workshops/myboiler/js/sandbox.js
+++ Workshops/myboiler/js/sandbox.js
@@ -1,1 +1,2 @@
-// My JS code - boiler
+// My code
+
--- Workshops/netninja add class challenge/js/sandbox.js
+++ Workshops/netninja add class challenge/js/sandbox.js
@@ -1,1 +1,12 @@
-// My JS code - boiler
+const allPara = document.querySelectorAll('p');
+
+allPara.forEach(paragraph => {
+    
+    let content = paragraph.innerText;
+
+    if (content.includes('success')) {
+        paragraph.classList.add('success');
+    } else if (content.includes('error')) {
+        paragraph.classList.add('error');
+    }
+});
