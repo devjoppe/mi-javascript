46a8fbe9fc88ea6e3b5e0064d516553d8bc0d5e0
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-01 21:24:49 +0100

    Removed API Key

--- Workshops/36-weather-app/assets/js/owmapi.js
+++ Workshops/36-weather-app/assets/js/owmapi.js
@@ -1,6 +1,6 @@
 /**
  * 🌧️.
- * https://api.openweathermap.org/data/2.5/weather?q=Malmö&units=metric&appid=68eaaf9450df25dd20d8266693b09ddc
+ * https://api.openweathermap.org/data/2.5/weather?q=Malmö&units=metric&appid=MYAPI
  */
 
 
